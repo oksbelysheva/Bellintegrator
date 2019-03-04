17bc9d48a1b18a548a8bb8810cec3db09b8b6374
oksbelysheva <oks.belysheva@yandex.ru>
2019-03-04 22:33:50 +0400

    let/var

--- task3/script.js
+++ task3/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
-	var available = document.getElementsByClassName('blockList available')[0].getElementsByTagName('form')[0].elements[0],
+	let available = document.getElementsByClassName('blockList available')[0].getElementsByTagName('form')[0].elements[0],
 	btnRight = document.getElementsByClassName('btnRight')[0],
 	btnAllRight = document.getElementsByClassName('btnAllRight')[0],
 	btnLeft = document.getElementsByClassName('btnLeft')[0],
@@ -30,8 +30,8 @@ window.addEventListener('DOMContentLoaded', function() {
 		removeWarning();
 
 		let warningFlag = true;
-		for (var i = 0; i < from.options.length; i++) {
-			var option = from.options[i];
+		for (let i = 0; i < from.options.length; i++) {
+			let option = from.options[i];
 			if(option.selected) {
 				i--;
 				warningFlag = false;
@@ -49,7 +49,7 @@ window.addEventListener('DOMContentLoaded', function() {
 		removeWarning();
 
 		let elements = from.options;
-		for (var i = 0; i < elements.length; i++) {
+		for (let i = 0; i < elements.length; i++) {
 				let currentOption = elements[i--];
 				to.appendChild(currentOption);
 				currentOption.selected ="";
